da62bb7e2ea78903cf2244afcf4ef101297e83a8
Srikanth Dumpeti <d.sri6365@gmail.com>
2023-10-11 01:16:27 +0530

    Added @ResponseStatus Annotation.

--- src/main/java/com/myproject/ecommerce/Exceptions/ProductNotFoundException.java
+++ src/main/java/com/myproject/ecommerce/Exceptions/ProductNotFoundException.java
@@ -1,5 +1,9 @@
 package com.myproject.ecommerce.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)   // Whenever ProductNotFoundException occurs this will send response status as HttpStatus.NOT_FOUND
 public class ProductNotFoundException extends RuntimeException{
 
     public ProductNotFoundException(){
